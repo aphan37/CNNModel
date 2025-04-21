c4acab89058d41c8b7e4f6f3f12c0eab78eba9f0
Anh Phan <hphan3113@gmail.com>
2025-04-20 21:25:15 -0400

    Create AppModel.py

---
+++ AppModel.py
@@ -0,0 +1,30 @@
+# cnn_app_model.py
+import torch.nn as nn
+import torch.nn.functional as F
+
+class AlzhiNet(nn.Module):
+    def __init__(self, num_classes):
+        super(AlzhiNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(256 * 28 * 28, 1024)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.flatten(x)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
+        return x
